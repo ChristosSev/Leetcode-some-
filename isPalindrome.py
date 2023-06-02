e6963e01d80af158e39b326f3dc3b12d623dac83
Christos Sevastopoulos <45820335+ChristosSev@users.noreply.github.com>
2023-06-02 12:49:23 -0500

    Update isPalindrome.py

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,21 +1,21 @@
 import re
 
-def isPalindrome(s: str) -> bool:
-    clean_string = re.sub(r'[\W_]+', '', s)
-    lower_s = clean_string.lower()
+def isPalindrome (s:str) -> bool:
+    str = s.lower()
+    str= re.sub(r'[\W_]', '', str)
+    #print(str)
     l = 0
-    r = len(lower_s) - 1
+    r = len(str) - 1
 
     while l < r:
-        if lower_s[l] != lower_s[r]:
-            return False
-
-        l += 1
-        r -= 1
-
+          print(r)
+          print(l)
+          if str[l] != str[r]:
+             return False
+          l+=1
+          r-=1
     return True
 
-string = "A man, a plan, a canal, Panama!"
-result = isPalindrome(string)
-print(result)  # Output: True
 
+str ='Bia_BIA'
+print(isPalindrome(str))
